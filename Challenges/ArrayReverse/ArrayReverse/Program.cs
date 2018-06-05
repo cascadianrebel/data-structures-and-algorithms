57b11bd21bdd77a429183d41a5e93744f2e7a766
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-05 09:20:43 -0700

    added correct folders

---
+++ Challenges/ArrayReverse/ArrayReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArrayReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        int[] arr = {0,1,2,3,4,5};
+        int[] arrNew = new int[arr.length];
+
+        int j = 0;
+
+            for(int i = arr1.Length - 1; i >= 0; i--)
+            {
+              arr2[j] = arr1[i];
+              j++;
+            }
+}
+}
