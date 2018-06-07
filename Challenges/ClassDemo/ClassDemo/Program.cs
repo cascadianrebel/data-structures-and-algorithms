d14171b1b4a17b235814467fb523ff1ed7c63bc1
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-06 23:07:18 -0700

    added whiteboard photo

--- Challenges/ClassDemo/ClassDemo/Program.cs
+++ Challenges/ClassDemo/ClassDemo/Program.cs
@@ -7,14 +7,54 @@ namespace ClassDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine($"What is your favorite color");
-            string color = Console.ReadLine();
-            FavoriteColor(color);
+            //TryBlockExample();
+            CatchSingleException();
         }
-        static void FavoriteColor(string color)
+        
+        static void TryBlockExample()
         {
-            Console.WriteLine($"Your favorite color is {color}");
+            string number = "twenty";
+            try
+            {
+                int twenty = Convert.ToInt32(number);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"{number} is not an integer");
+                Console.WriteLine(e.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("This is an exception for Null references");
+            }
+                
+            catch (Exception ex)
+            {
+                Console.WriteLine("This is an exception");
+            }
 
         }
+
+        static void CatchSingleException()
+        {
+            int milesDriven, gallonsOfGas, mpg;
+
+            try
+            {
+                Console.WriteLine("Enter Miles Driven: ");
+                milesDriven = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter gallons of gas purchased: ");
+                gallonsOfGas = Convert.ToInt32(Console.ReadLine());
+
+                mpg = milesDriven / gallonsOfGas;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                //throw;
+            }
+        }
+
     }
 }
