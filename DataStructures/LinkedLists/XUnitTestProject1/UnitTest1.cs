53547b331d7faef158fd2dba659d00974bc69ece
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-13 17:11:49 -0700

    added tests

--- DataStructures/LinkedLists/XUnitTestProject1/UnitTest1.cs
+++ DataStructures/LinkedLists/XUnitTestProject1/UnitTest1.cs
@@ -46,5 +46,28 @@ namespace XUnitTestProject1
             //Assert
             Assert.Equal(expected, found.Value);
         }
+        [Theory]
+        [InlineData(42)]
+        [InlineData(100)]
+        public void ReturnsNullForNodeThatDoesNotExist(int value)
+        {
+            //Arrange
+            LinkList ll = new LinkList(new Node(4));
+            Node node = new Node(8);
+            Node node2 = new Node(15);
+            Node node3 = new Node(16);
+            Node node4 = new Node(23);
+
+            //Act
+            ll.Add(node);
+            ll.Add(node2);
+            ll.Add(node3);
+            ll.Add(node4);
+
+            Node found = ll.Find(value);
+
+            //Assert
+            Assert.Null(found);
+        }
     }
 }
