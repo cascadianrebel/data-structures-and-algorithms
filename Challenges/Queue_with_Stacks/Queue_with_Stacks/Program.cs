ea529f73398c1981add9f0735b63ac29f8520a2b
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-19 11:55:14 -0700

    added classes

--- Challenges/Queue_with_Stacks/Queue_with_Stacks/Program.cs
+++ Challenges/Queue_with_Stacks/Queue_with_Stacks/Program.cs
@@ -8,5 +8,21 @@ namespace Queue_with_Stacks
         {
             Console.WriteLine("Hello World!");
         }
+        static void QueueWithStacks()
+        {
+            // create nodes to add to stack
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+            // add first node to stack
+            Stack stack1 = new Stack(node1);
+
+            //push nodes to stack
+
+            //pop nodes to new stack in reverse order
+
+            
+
+        }
     }
 }
