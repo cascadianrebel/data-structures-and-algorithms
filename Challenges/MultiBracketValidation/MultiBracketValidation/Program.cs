d4fe58b4ea4d51cbaad1c905fdfce285c03d7d95
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-20 16:20:01 -0700

    moved mergeLists to challenge folder

---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultiBracketValidation
+{
+    class Program
+    {
+        public static string bracket1 = "{}{Code}[Fellows](())";
+        public static string bracket2 = "[({}]";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MultiBracketValidation(bracket1);
+        }
+
+
+       public static List<string> SquareList = new List<string>();
+       public static  List<string> CurlyList = new List<string>();
+       public static List<string> RoundList = new List<string>();
+
+        public static bool MultiBracketValidation(string input)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+               if (input[i].ToString() is "(" || input[i].ToString() is ")")
+                {
+                    SquareList.Add(input[i].ToString());
+                }
+                if (input[i].ToString() is "{" || input[i].ToString() is "}")
+                {
+                    CurlyList.Add(input[i].ToString());
+                }
+                if (input[i].ToString() is "[" || input[i].ToString() is "]")
+                {
+                    RoundList.Add(input[i].ToString());
+                }
+            }
+            ;
+            Console.WriteLine(SquareList);
+            Console.WriteLine(RoundList);
+            Console.WriteLine(CurlyList);
+
+
+            Console.Read();
+            return true;
+        }
+    }
+}
--- Challenges/fifo_animal_shelter/fifo_animal_shelter/Program.cs
+++ Challenges/fifo_animal_shelter/fifo_animal_shelter/Program.cs
@@ -7,10 +7,26 @@ namespace fifo_animal_shelter
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            FIFOAnimalShelter();
         }
 
         static void FIFOAnimalShelter()
         {
+            Animal pet1 = new Animal("cat");
+            Animal pet2 = new Animal("dog");
+            Animal pet3 = new Animal("dog");
+            Animal pet4 = new Animal("fish");
+            Animal Pet5 = new Animal("cat");
+
+
+            AnimalShelter animalShelter = new AnimalShelter(pet1);
+
+            animalShelter.Enqueue(pet2);
+            animalShelter.Enqueue(pet3);
+
+            Console.WriteLine(animalShelter.Dequeue("dog"));
+
+
 
         }
 
