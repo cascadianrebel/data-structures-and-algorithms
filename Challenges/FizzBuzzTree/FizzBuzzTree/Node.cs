955eae886d0144e41e3f7c0ae5b0bbac0de7a24e
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-27 11:18:32 -0700

    added code and images to readme

---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzTreeChallenge
+{
+    public class Node
+    {
+        public string Value {get; set;}
+        public Node RightChild { get; set; }
+        public Node LeftChild { get; set; }
+
+        public Node (string value)
+        {
+            Value = value;
+        }
+    }
+}
--- Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace FizzBuzzTree
+namespace FizzBuzzTreeChallenge
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fizz Buzz Tree!");
+            FizzBuzzTree();
+        }
+
+        public static Node FizzBuzzTree (Node root)
+        {
+           int rootInt = Convert.ToInt32(root.Value);
+
+            if (rootInt %5 == 0 && rootInt %3 == 0)
+            {
+                root.Value = "FizzBuzz";
+            }
+            else if (rootInt %5 == 0)
+            {
+                root.Value = "Buzz";
+            }
+            else if (rootInt %3 == 0)
+            {
+                root.Value = "Fizz";
+            }
+            if (root.LeftChild != null)
+            {
+                FizzBuzzTree(root.LeftChild);
+            }
+            if (root.RightChild != null)
+            {
+                FizzBuzzTree(root.RightChild);
+            }
+            return root;
         }
     }
 }
