eae9034cec82a7611cb47d5e03b8f98518693314
cascadianrebel <Anthonydgreen90@gmail.com>
2018-07-16 11:39:27 -0700

    initial

---
+++ DataStructures/HashTables/HashTables/Bucket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTables
+{
+
+    public class Bucket
+    {
+        /// <summary>
+        /// properties shared by buckets in hashtables
+        /// </summary>
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public Bucket Next { get; set; }
+    }
+}
