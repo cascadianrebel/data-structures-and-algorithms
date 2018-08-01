6134f5ba821b06cfa38f989107865cbef5605554
cascadianrebel <Anthonydgreen90@gmail.com>
2018-07-31 23:39:54 -0700

    finished

--- DataStructures/HashTables/XUnitTestProject1/UnitTest1.cs
+++ DataStructures/HashTables/XUnitTestProject1/UnitTest1.cs
@@ -9,13 +9,60 @@ namespace XUnitTestProject1
         [Fact]
         public void CanAddBucketToTable()
         {
-            HashTables.Hashtable testHashtable = new HashTables.Hashtable();
+            Hashtable testTable = new Hashtable();
 
-            testHashtable.AddBucket("testKey", "testValue");
+            testTable.AddBucket("TestKey", "TestValue");
 
-            int
-                indexTest = Hashtable.GetHash("testKey");
-
+            Assert.Equal("TestValue", testTable.Find("TestKey"));
         }
+
+        [Fact]
+        public void CanCheckTableToContainKey()
+        {
+            Hashtable testTable = new Hashtable();
+
+            testTable.AddBucket("TestKey", "TestValue");
+
+            Assert.True(testTable.Contains("TestKey"));
+        }
+
+        [Fact]
+        public void CollisionsAllowed()
+        {
+            Hashtable testTable = new Hashtable();
+
+            testTable.AddBucket("TestKey", "TestValue");
+
+            testTable.AddBucket("TestKey", "Collision!");
+
+            int testBucket = testTable.GetHash("TestKey");
+
+            Assert.Equal("TestValue", testTable.BucketArray[testBucket].Next.Value);
+
+            Assert.Equal("Collision!", testTable.BucketArray[testBucket].Value);
+        }
+
+        [Fact]
+        public void MultipleCollisionsAllowed()
+        {
+            Hashtable testTable = new Hashtable();
+
+            testTable.AddBucket("TestKey", "TestValue");
+
+            testTable.AddBucket("TestKey", "Collision!");
+
+            testTable.AddBucket("TestKey", "Another Collision!");
+
+            int testBucket = testTable.GetHash("TestKey");
+
+            Assert.Equal("TestValue", testTable.BucketArray[testBucket].Next.Next.Value);
+
+            Assert.Equal("Collision!", testTable.BucketArray[testBucket].Next.Value);
+
+            Assert.Equal("Another Collision!", testTable.BucketArray[testBucket].Value);
+        }
+
+
+
     }
 }
